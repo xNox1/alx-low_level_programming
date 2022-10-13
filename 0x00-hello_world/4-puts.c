955e66d6186a4eb9185d40a0fb72015d7f2cb701
xNox1 <meddaou2003@gmail.com>
2022-10-13 16:02:17 -0700

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - Entry
+ * Return: Always 0
+ */
 
 int main(void)
 
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("The size of a char : %i byte(s)\n", sizeof(char));
+	printf("The size of a int : %i byte(s)\n", sizeof(int));
+	printf("The size of a long int : %i byte(s)\n", sizeof(long int));
+	printf("The size of a long long int : %i byte(s)\n", sizeof(long long int));
+	printf("The size of a float : %i byte(s)\n", sizeof(float));
+	return (0);
+}	
