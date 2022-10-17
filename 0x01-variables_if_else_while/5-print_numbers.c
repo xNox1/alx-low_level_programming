efa01e35595004f39f16db3434fee144a00e3786
xNox1 <meddaou2003@gmail.com>
2022-10-17 10:33:28 -0700

    tasks

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - tasks
+ * Return: Always 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf('\n');
+	return (0);
+}
